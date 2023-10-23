afe8f779a9e1a134cfabebb2ef7d78cdc64370f2
vajki8 <73273850+vajki8@users.noreply.github.com>
2023-10-23 10:31:13 +0200

    Add project files.

---
+++ WDCO2R_HFT_2023241/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WDCO2R_HFT_2023241
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
