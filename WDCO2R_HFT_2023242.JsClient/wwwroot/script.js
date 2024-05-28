c101dc0b17998b34fdd87b49b856ef99f8e94a81
vajki8 <vajk.olbey@gmail.com>
2024-05-28 16:25:11 +0200

    js

--- WDCO2R_HFT_2023242.JsClient/wwwroot/script.js
+++ WDCO2R_HFT_2023242.JsClient/wwwroot/script.js
@@ -1,36 +1,49 @@
-let rentals = [];
+let boardgames = [];
 let connection = null;
-let noncrudarray = [];
-
-let rentalIdToUpdate = -1;
 getdata();
-setupSignalR();
+setupsignalr();
 
-function setupSignalR() {
+let boardgameidtoupdate = -1;
+
+async function getdata() {
+    await fetch('http://localhost:35357/boardgame').then(x => x.json()).then(y => {
+        boardgames = y;
+        display();
+    });
+}
+
+function setupsignalr() {
     connection = new signalR.HubConnectionBuilder()
         .withUrl("http://localhost:35357/hub")
         .configureLogging(signalR.LogLevel.Information)
         .build();
 
-    connection.on("rentalCreated", (user, message) => {
-        getdata();
-    });
+    connection.on
+        (
+            "boardgameCreated", (user, message) => {
+                getdata();
+            });
 
-    connection.on("rentalDeleted", (user, message) => {
-        getdata();
-    });
 
-    connection.on("rentalUpdated", (user, message) => {
-        getdata();
-    });
+    connection.on
+        (
+            "boardgameDeleted", (user, message) => {
+                getdata();
+            });
 
-    connection.onclose(async () => {
-        await start();
-    });
+    connection.on
+        (
+            "boardgameUpdated", (user, message) => {
+                getdata();
+            });
+
+    connection.onclose
+        (async () => {
+            await start();
+        });
     start();
 
 }
-
 
 async function start() {
     try {
@@ -42,46 +55,28 @@ async function start() {
     }
 };
 
-
-async function getdata() {
-    await fetch('http://localhost:35357/rental')
-        .then(x => x.json())
-        .then(y => {
-            rentals = y;
-            display();
-        });
+function showupdate(id) {
+    document.getElementById('gametitleupdate').value = boardgames.find(x => x['boardgameId'] == id)['title'];
+    document.getElementById('gametypeupdate').value = boardgames.find(x => x['boardgameId'] == id)['type'];
+    document.getElementById('updateformdiv').style.display = 'flex';
+    boardgameidtoupdate = id;
 }
-
 
 function display() {
     document.getElementById('resultarea').innerHTML = "";
-    rentals.forEach(t => {
-        document.getElementById('resultarea').innerHTML +=
-            "<tr><td>" + t.rentalId + "</td><td>"
-            + t.rentalName + "</td><td>" +
-            `<button type="button" onclick="remove(${t.rentalId})">Delete</button>` +
-            `<button type="button" onclick="showupdate(${t.rentalId})">Update</button>`
-            + "</td></tr>";
+    boardgames.forEach(x => {
+        document.getElementById('resultarea').innerHTML += "<tr><td>" + x.title + "</td><td>" + x.type + "</td><td>" + `<button type="button" onclick="remove(${x.boardgameId})">Delete</button>` + `<button type="button" onclick="showupdate(${x.boardgameId})">Update</button>` + "</td></tr>"
+        console.log(x.title);
     });
 }
 
-function showupdate(id) {
-    document.getElementById('rentalnametoupdate').value = rentals.find(t => t['rentalId'] == id)['rentalName'];
-    document.getElementById('updateformdiv').style.display = 'flex';
-    rentalIdToUpdate = id;
-}
-
-function update() {
-    document.getElementById('updateformdiv').style.display = 'none';
-    let name = document.getElementById('rentalnametoupdate').value;
-    fetch('http://localhost:35357/rental', {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json', },
-        body: JSON.stringify(
-            {
-                rentalName: name,
-                rentalId: rentalIdToUpdate
-            }),
+function remove(id) {
+    fetch('http://localhost:35357/boardgame/' + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: null
     })
         .then(response => response)
         .then(data => {
@@ -94,13 +89,18 @@ function update() {
 }
 
 function create() {
-    let name = document.getElementById('rentalname').value;
-    fetch('http://localhost:35357/rental', {
+    let ttitle = document.getElementById("gametitle").value;
+    let ttype = document.getElementById("gametype").value;
+
+    fetch('http://localhost:35357/boardgame', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json', },
+        headers: {
+            'Content-Type': 'application/json',
+        },
         body: JSON.stringify(
             {
-                rentalName: name
+                type: ttype,
+                title: ttitle
             }),
     })
         .then(response => response)
@@ -111,47 +111,35 @@ function create() {
         .catch((error) => {
             console.error('Error:', error);
         });
+
+
 }
 
-function remove(id) {
-    fetch('http://localhost:35357/rental/' + id, {
-        method: 'DELETE',
-        headers: { 'Content-Type': 'application/json', },
-        body: null
+function update() {
+    document.getElementById('updateformdiv').style = 'none';
+    let ttitle = document.getElementById("gametitleupdate").value;
+    let ttype = document.getElementById("gametypetoupdate").value;
+
+    fetch('http://localhost:35357/boardgame', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            {
+                type:ttype,
+                title: ttitle,
+                boardgameId: boardgameidtoupdate
+            }),
     })
         .then(response => response)
         .then(data => {
             console.log('Success:', data);
             getdata();
         })
-        .catch((error) => { console.error('Error:', error); });
-}
-
-function noncrud() {
-    fetch('http://localhost:35357/MatchStat/AverageOddsByrental')
-        .then(x => x.json())
-        .then(y => {
-            noncrudarray = y;
-            var table = document.getElementById("table");
-            table.innerHTML = "";
-            table.style.width = "100%";
-            var tr = document.createElement("tr");
-            var th1 = document.createElement("th");
-            var th2 = document.createElement("th");
-            th1.innerHTML = "Name";
-            th2.innerHTML = "Odds";
-            tr.appendChild(th1);
-            tr.appendChild(th2);
-            table.appendChild(tr);
-            for (var i = 0; i < y.length; i++) {
-                var tr = document.createElement("tr");
-                var td1 = document.createElement("td");
-                var td2 = document.createElement("td");
-                td1.innerHTML = y[i].name;
-                td2.innerHTML = y[i].odds;
-                tr.appendChild(td1);
-                tr.appendChild(td2);
-                table.appendChild(tr);
-            }
+        .catch((error) => {
+            console.error('Error:', error);
         });
+
+
 }
